87b70dc46af08007536cb70ad8677ea24b5e7afd
Ludmila2603 <mika22105@gmail.com>
2020-11-02 09:36:39 +0300

    modefied webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,56 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-	mode: 'development',
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.png|.woff|.ttf|.eot|.woff2$/,
-				use: [
-					'file-loader',
-					{
-						loader: 'image-webpack-loader',
-					},
-				],
-			},
-			{
-				test: /\.svg$/,
-				use: ['@svgr/webpack', 'url-loader'],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-		}),
-		new FaviconsWebpackPlugin('./public/favicon.ico'),
-	],
-	devServer: {
-		historyApiFallback: true,
-	},
-	externals: {
-		// global app config object
-		config: JSON.stringify({
-			apiUrl: 'http://localhost:4000',
-		}),
-	},
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.png|.woff|.ttf|.eot|.woff2$/,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svg/webpack", "url-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new FaviconsWebpackPlugin("./public/favicon.ico"),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: "http://localhost:4000",
+    }),
+  },
 };
