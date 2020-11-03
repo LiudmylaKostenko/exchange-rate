3ae3afbeca490ecfddfc9dcd4321d72623ece4e6
Ludmila2603 <mika22105@gmail.com>
2020-11-03 12:01:28 +0300

    changed view of day  time when updating

--- src/ui/components/ExchangeRatePage/ExchangeRatePageContainer.js
+++ src/ui/components/ExchangeRatePage/ExchangeRatePageContainer.js
@@ -153,9 +153,9 @@ class ExchangeRatePageContainer extends React.Component {
 
 	onDateTimeUpdate = () => {
 		const date = new Date();
-		const month = `0${date.getMonth() + 1}`;
+		const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
+		const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
 		const year = date.getFullYear();
-		const day = date.getDate();
 		const hour = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`;
 		const minute = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`;
 		const seconds = date.getSeconds() > 9 ? date.getSeconds() : `0${date.getSeconds()}`;
